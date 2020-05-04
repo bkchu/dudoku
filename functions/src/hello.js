57543724383d5e736919139de4bc5214856ac4ff
Brandon Chung <brandon.k.chung@gmail.com>
2020-05-03 20:52:10 -0500

    bootstrap netlify functions

---
+++ functions/src/hello.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export async function handler(event, context, callback) {
+  console.log("queryStringParameters", event.queryStringParameters)
+  const { data } = await axios.get("https://sugoku.herokuapp.com/board?difficulty=easy");
+  callback(null, {
+    // return null to show no errors
+    statusCode: 200, // http status code
+    body: JSON.stringify(data),
+  })
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,17 @@
+const { createProxyMiddleware } = require("http-proxy-middleware") //v1.x.x
+
 module.exports = {
+  developMiddleware: app => {
+    app.use(
+      "/.netlify/functions/",
+      createProxyMiddleware({
+        target: "http://localhost:9000",
+        pathRewrite: {
+          "/.netlify/functions/": "",
+        },
+      })
+    )
+  },
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
