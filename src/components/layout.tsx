7bf7673ee347801c68acd9b4cc8a5b2e2bb876d0
Brandon Chung <brandon.k.chung@gmail.com>
2020-05-08 23:14:04 -0500

    fix casing

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./Layout.css"
+import "./layout.css"
 import '../theme/index.scss';
 
 const Layout = ({ children }) => {
