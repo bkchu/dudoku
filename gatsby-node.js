7b2246f4f7e08838d54070f5e11c4becd903b255
Brandon Chung <brandon.k.chung@gmail.com>
2020-05-09 13:53:17 -0500

    remove post build

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,26 +3,26 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-const glob = require('glob');
-const path = require('path');
-const fs = require('fs');
+// const glob = require('glob');
+// const path = require('path');
+// const fs = require('fs');
 
-exports.onPostBuild = () => {
-  // Configure where the functions are kept and where we want to move them.
-  const srcLocation = `${__dirname}/static/functions`;
-  const outputLocation = `${__dirname}/public/functions`;
+// exports.onPostBuild = () => {
+//   // Configure where the functions are kept and where we want to move them.
+//   const srcLocation = `${__dirname}/static/functions`;
+//   const outputLocation = `${__dirname}/public/functions`;
 
-  // Get all the functions.
-  const modules = glob.sync('*.js', { cwd: srcLocation });
-  modules.forEach(src => {
-    const moduleSrc = path.join(srcLocation, src);
-    const moduleOut = path.join(outputLocation, path.basename(src, path.extname(src)) + '.js');
+//   // Get all the functions.
+//   const modules = glob.sync('*.js', { cwd: srcLocation });
+//   modules.forEach(src => {
+//     const moduleSrc = path.join(srcLocation, src);
+//     const moduleOut = path.join(outputLocation, path.basename(src, path.extname(src)) + '.js');
 
-    // Copy file to new location.
-    fs.copyFile(moduleSrc, moduleOut, (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-  });
-};
+//     // Copy file to new location.
+//     fs.copyFile(moduleSrc, moduleOut, (err) => {
+//       if (err) {
+//         throw err;
+//       }
+//     });
+//   });
+// };
