b59a345b134060fd84785577b362cfa555e73eec
Brandon Chung <brandon.k.chung@gmail.com>
2020-05-08 23:19:39 -0500

    functions on gatbsy netlify

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,26 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const glob = require('glob');
+const path = require('path');
+const fs = require('fs');
 
-// You can delete this file if you're not using it
+exports.onPostBuild = () => {
+  // Configure where the functions are kept and where we want to move them.
+  const srcLocation = `${__dirname}/functions`;
+  const outputLocation = `${__dirname}/public/functions`;
+
+  // Get all the functions.
+  const modules = glob.sync('*.js', { cwd: srcLocation });
+  modules.forEach(src => {
+    const moduleSrc = path.join(srcLocation, src);
+    const moduleOut = path.join(outputLocation, path.basename(src, path.extname(src)) + '.js');
+
+    // Copy file to new location.
+    fs.copyFile(moduleSrc, moduleOut, (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+  });
+};
