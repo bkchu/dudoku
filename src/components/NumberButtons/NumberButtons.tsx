16b84dea5ea73f47910f0cdffd04bdcba0c36685
Brandon Chung <brandon.k.chung@gmail.com>
2020-05-13 23:27:27 -0500

    css fixes

--- src/components/NumberButtons/NumberButtons.tsx
+++ src/components/NumberButtons/NumberButtons.tsx
@@ -5,30 +5,37 @@ import { createBoardSetPaintNumberAction } from "../../governor/actions"
 import { InitialState } from "../../governor/initialState"
 import { selectActivePieceIndex } from "../../governor/selectors"
 
-import './NumberButtons.css';
+import "./NumberButtons.css"
 
 export interface NumberButtonsProps {
   onNumberButtonPress: Function
   activePieceIndex: number
 }
 
-const NumberButtons: FC<NumberButtonsProps> = ({ onNumberButtonPress, activePieceIndex }) => {
-  const buttons = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "X",
-  ].map(key => (
-    <button className="number-buttons__button" key={key} onClick={() => activePieceIndex != null && onNumberButtonPress(key === "X" ? 0 : +key)}>{key}</button>
-  ))
+const NumberButtons: FC<NumberButtonsProps> = ({
+  onNumberButtonPress,
+  activePieceIndex,
+}) => {
+  const buttons = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "X"].map(
+    key => (
+      <button
+        className="number-buttons__button"
+        key={key}
+        onClick={() =>
+          activePieceIndex != null &&
+          onNumberButtonPress(key === "X" ? 0 : +key)
+        }
+      >
+        {key}
+      </button>
+    )
+  )
 
-  return <div className="number-buttons">{buttons}</div>
+  return (
+    <div className="number-buttons">
+      <div className="number-buttons__container">{buttons}</div>
+    </div>
+  )
 }
 const mapStateToProps = (state: InitialState) => ({
   activePieceIndex: selectActivePieceIndex(state),
